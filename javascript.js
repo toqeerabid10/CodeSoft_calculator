8d6439e69fcec96218b5fd54494ae31473bd00fc
Toqeer Ahmad <136381451+toqeerabid10@users.noreply.github.com>
2023-08-12 16:14:57 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,20 @@
+let string = "";
+let buttons = document.querySelectorAll('.button,.operator');
+
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            document.querySelector('input').value = string;
+        } else if (e.target.innerHTML == 'AC') {
+            string = "";
+            document.querySelector('input').value = string;
+        } else if (e.target.innerHTML == 'DEL') {
+            string = string.substring(0, string.length - 1);
+            document.querySelector('input').value = string;
+        } else {
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+    })
+});
